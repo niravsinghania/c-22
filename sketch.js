44a4a9b9a4be871d2fccb3dd53e0a0a33acec95a
niravsinghania <76439001+niravsinghania@users.noreply.github.com>
2021-01-21 16:25:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+	fairyVoice.play();
+	
+	
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+keyPressed();
+function draw() {
+  background(bgImg);
+
+  //fairy.x = World.mousex.X;
+
+  //keyPressed();
+  drawSprites();
+
+}
+
+function keyPressed() {
+	//write code here
+	if (star.keyDown("LEFT_ARROW")) {
+		fairy.velocity=2;
+	}
+}
